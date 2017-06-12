01da820d3ddd93fb059cc5ae049080dc11bfd3c7
Hitoshi Inasa <hitoshi-inasa@dac.co.jp>
2017-06-12 10:06:53 +0000

    modified secure

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -1,5 +1,5 @@
 class TasklistsController < ApplicationController
-  before_action :require_user_logged_in, only: [:new, :create]
+  before_action :require_user_logged_in, only: [:show, :new, :create, :edit,]
   before_action :set_tasklist, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  get 'logout', to: 'sessions#new'
+#  get 'logout', to: 'sessions#new'
   get 'signup', to: 'users#new'
 #  post 'signup', to: 'tasklists#index'
   resources :users, only: [:new, :create]
